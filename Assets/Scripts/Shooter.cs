6a36b75b9c9c49079036f9e1252c4faba527918c
riekure <s2darl0uvgia24@gmail.com>
2018-12-16 23:28:30 +0900

    add Shooter

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    // Candyプレファブパラメータ
+    public GameObject candyPrefab;
+    public float shotSpeed;
+    public float shotTorque;
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetButtonDown("Fire1"))
+        {
+            Shot();
+        }	
+	}
+
+    public void Shot()
+    {
+        // プレファブからCandyオブジェクトを生成
+        GameObject candy = (GameObject)Instantiate(candyPrefab, transform.position, Quaternion.identity);
+
+        // CandyオブジェクトのRigidbodyを取得し力と回転を加える
+        Rigidbody candyRigidBody = candy.GetComponent<Rigidbody>();
+        candyRigidBody.AddForce(transform.forward * shotSpeed);
+        candyRigidBody.AddTorque(new Vector3(0, shotTorque, 0));
+    }
+}
